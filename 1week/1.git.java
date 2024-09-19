50c70ed4e90835031d3745fe96e9fcd1f50e3208
Yoonssu <orvosdoktor21@naver.com>
2024-09-19 10:27:51 +0900

    1week

---
+++ 1week/1.git.java
@@ -0,0 +1,32 @@
+/*1. github 
+
+- 코드 변경점을 기록하는 것이다.
+
+2. 필수 git 명령어
+
+1) git init : 깃 초기화 및 코드 관리 
+2) git add : 로컬에 있는 작업물 추가
+-> add . : 작업물 전부 다 업로드
+
+3) git commit -m "blabla" : 깃 커밋
+4) git status : 현재 깃에서 자신의 위치가 어디에 있는지
+
+5) git push origin main : 깃 커밋 후 작업물 깃에 푸시
+
+6) git log : 변경 기록 내역 확인
+
+
+7) git branch : 설정한 이름으로 branch를 만들어 main에 
+병합하기 전에 해당 branch로 작업물 업로드
+
+-> 작업물을 관리하기 용이함.
+
+8) git checkout : 현재 브랜치에서 다른 브랜치로 이동하고 싶을 때 사용
+
+9) git pull : 변경된 코드를 내 컴퓨터에 반영할 수 있음. 
+
+10) pull request & git merge : pull reuest 
+
+
+
+*/
